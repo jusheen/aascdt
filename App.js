0ddc82ed6b2c37fbd51c4ab0134cb35580d15de4
jusheen <jusheen9@gmail.com>
2020-09-13 16:02:27 -0700

    trying to fix null is not an object error. likely gesture handler bug

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-// import 'react-native-gesture-handler';
+import 'react-native-gesture-handler';
 import React from 'react';
 import { Text, View, StyleSheet } from 'react-native';
 import { createStackNavigator } from 'react-navigation-stack';
