a40756da178f1d2252134c9baafe01b482370dd5
jusheen <jusheen9@gmail.com>
2020-09-14 11:44:49 -0700

    added 4 cafeteria cards. about to refactor and modularize and separate screens from components

--- src/components/Cafeterias.js
+++ src/components/Cafeterias.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { View, Text, Image, StyleSheet, StatusBar } from 'react-native';
+import { View, Text, Image, StyleSheet, StatusBar, ScrollView } from 'react-native';
 import { NavigationContainer, useTheme } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import Swiper from 'react-native-swiper';
@@ -11,52 +11,127 @@ const Tab = createBottomTabNavigator();
 const CafeteriasScreen = () => {
   const theme = useTheme();
   return (
-    <View style={styles.container}>
-      <StatusBar barStyle={theme.dark ? 'light-content' : 'dark-content'}/>
-      <View style={styles.sliderContainer}>
-        <Swiper
-          autoplay
-          autoplayTimeout={5}
-          height={200}
-        >
-          <View style={styles.slide}>
-            <Image
-              source={require('../images/aascdt-banner.png')}
-              resizeMode='cover'
-              style={styles.sliderImage}
-            />
-          </View>
-          <View style={styles.slide}>
-            <Image
-              source={require('../images/founder.png')}
-              resizeMode='cover'
-              style={styles.sliderImage}
-            />
-          </View>
-          <View style={styles.slide}>
-            <Image
-              source={require('../images/bears_eating_together.jpg')}
-              resizeMode='cover'
-              style={styles.sliderImage}
-            />
-          </View>
-        </Swiper>
-      </View>
+    <ScrollView>
+      <View style={styles.container}>
+        <StatusBar barStyle={theme.dark ? 'light-content' : 'dark-content'}/>
+        <View style={styles.sliderContainer}>
+          <Swiper
+            autoplay
+            autoplayTimeout={5}
+            height={200}
+          >
+            <View style={styles.slide}>
+              <Image
+                source={require('../images/aascdt-banner.png')}
+                resizeMode='cover'
+                style={styles.sliderImage}
+              />
+            </View>
+            <View style={styles.slide}>
+              <Image
+                source={require('../images/founder.png')}
+                resizeMode='cover'
+                style={styles.sliderImage}
+              />
+            </View>
+            <View style={styles.slide}>
+              <Image
+                source={require('../images/bears_eating_together.jpg')}
+                resizeMode='cover'
+                style={styles.sliderImage}
+              />
+            </View>
+          </Swiper>
+        </View>
 
-      <View style={styles.cardsWrapper}>
-        <Text>Crossroads</Text>
-        <View style={styles.card}>
-          <View style={styles.cardImgWrapper}>
-            <Image
-              source={require('../images/crossroads.png')}
-              resizeMode='cover'
-              style={styles.cardImg}
-            />
+        <View style={styles.cardsWrapper}>
+          <Text
+            style={{
+              alignSelf: 'center',
+              fontSize: 18,
+              fontWeight: 'bold',
+              color: '#333',
+            }}>
+            Dining Halls
+          </Text>
+          <View style={styles.card}>
+            <View style={styles.cardImgWrapper}>
+              <Image
+                source={require('../images/croads_2.jpg')}
+                resizeMode="cover"
+                style={styles.cardImg}
+              />
+            </View>
+            <View style={styles.cardInfo}>
+              <Text style={styles.cardTitle}>Crossroads</Text>
+              <Text style={styles.cardDetails}>
+                Fan favorite.
+              </Text>
+            </View>
+          </View>
+          <View style={styles.card}>
+            <View style={styles.cardImgWrapper}>
+              <Image
+                source={require('../images/cafe3.jpg')}
+                resizeMode="cover"
+                style={styles.cardImg}
+              />
+            </View>
+            <View style={styles.cardInfo}>
+              <Text style={styles.cardTitle}>Cafe 3</Text>
+              <Text style={styles.cardDetails}>
+                The infamous and notorious. At least it's vegan.
+              </Text>
+            </View>
+          </View>
+          <View style={styles.card}>
+            <View style={styles.cardImgWrapper}>
+              <Image
+                source={require('../images/i-house.jpg')}
+                resizeMode="cover"
+                style={styles.cardImg}
+              />
+            </View>
+            <View style={styles.cardInfo}>
+              <Text style={styles.cardTitle}>International House</Text>
+              <Text style={styles.cardDetails}>
+                Ah, I see you're a man of culture as well.
+              </Text>
+            </View>
+          </View>
+          <View style={styles.card}>
+            <View style={styles.cardImgWrapper}>
+              <Image
+                source={require('../images/clark-kerr.jpg')}
+                resizeMode="cover"
+                style={styles.cardImg}
+              />
+            </View>
+            <View style={styles.cardInfo}>
+              <Text style={styles.cardTitle}>Clark Kerr</Text>
+              <Text style={styles.cardDetails}>
+                Never been here, but heard it's pretty good.
+              </Text>
+            </View>
+          </View>
+          <View style={styles.card}>
+            <View style={styles.cardImgWrapper}>
+              <Image
+                source={require('../images/foothill.png')}
+                resizeMode="cover"
+                style={styles.cardImg}
+              />
+            </View>
+            <View style={styles.cardInfo}>
+              <Text style={styles.cardTitle}>Foothill</Text>
+              <Text style={styles.cardDetails}>
+                They have couches here, that's pretty cool.
+              </Text>
+            </View>
           </View>
         </View>
       </View>
-
-    </View>
+    </ScrollView>
   )
 };
 
