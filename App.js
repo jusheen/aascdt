090708efc301ff7ecc146d92e78aea2a6259c9a2
jusheen <jusheen9@gmail.com>
2020-09-13 18:47:12 -0700

    set up basic navigation structure

--- App.js
+++ App.js
@@ -1,17 +1,62 @@
+import 'react-native-gesture-handler';
 import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 import { Text, View, StyleSheet } from 'react-native';
 import Login from './src/components/Login/Login.js';
+
+const Stack = createStackNavigator();
+
+// const MyStack = () => {
+//   return (
+//     <NavigationContainer>
+//       <Stack.Navigator>
+//         <Stack.Screen
+//           name="Home"
+//           component={HomeScreen}
+//           options={{ title: 'Welcome' }}
+//         />
+//         <Stack.Screen name="Profile" component={ProfileScreen} />
+//       </Stack.Navigator>
+//     </NavigationContainer>
+//   );
+// };
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator
+        screenOptions={{
+          headerShown: false
+        }}
+      >
+        <Stack.Screen
+          name="Login"
+          component={Login}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
 
 const styles = StyleSheet.create({
   center: {
     alignItems: 'center',
   },
 });
+// export default function App() {
+//   return (
+//     <NavigationContainer>
+//       <Login />
+//     </NavigationContainer>
+//   );
+// }
+// const App = () => {
+//   return (
+//     <Login />
+//   )
+// };
 
-const App = () => {
-  return (
-    <Login />
-  )
-};
+// export default App;
 
-export default App;
+
