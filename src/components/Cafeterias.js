eb392e9987879fcf8a032a286abefea02d2d8b54
jusheen <jusheen9@gmail.com>
2020-09-14 00:28:00 -0700

    made slider component, currently only occupies 90% of screen. might change toi take up 100%

--- src/components/Cafeterias.js
+++ src/components/Cafeterias.js
@@ -1,27 +1,75 @@
 import React, { Component } from 'react';
-import { View, Text, StyleSheet } from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
+import { View, Text, Image, StyleSheet, StatusBar } from 'react-native';
+import { NavigationContainer, useTheme } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import Swiper from 'react-native-swiper';
 import Profile from './Profile';
 
 const Tab = createBottomTabNavigator();
+//
 
 const CafeteriasScreen = () => {
+  const theme = useTheme();
   return (
     <View style={styles.container}>
-      <Text>Cafeterias Screen</Text>
+      <StatusBar barStyle={theme.dark ? 'light-content' : 'dark-content'}/>
+      <View style={styles.sliderContainer}>
+        <Swiper
+          autoplay
+          autoplayTimeout={5}
+          height={200}
+        >
+          <View style={styles.slide}>
+            <Image
+              source={require('../images/aascdt-banner.png')}
+              resizeMode='cover'
+              style={styles.sliderImage}
+            />
+          </View>
+          <View style={styles.slide}>
+            <Image
+              source={require('../images/aascdt-banner.png')}
+              resizeMode='cover'
+              style={styles.sliderImage}
+            />
+          </View>
+        </Swiper>
+      </View>
     </View>
   )
 };
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#ffdd59'
-  },
-});
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     justifyContent: 'center',
+//     alignItems: 'center',
+//     backgroundColor: '#ffdd59'
+//   },
+//   sliderContainer: {
+//     height: 200,
+//     width: '90%',
+//     marginTop: 10,
+//     justifyContent: 'center',
+//     alignSelf: 'center',
+//     borderRadius: 8,
+//   },
+
+//   wrapper: {},
+
+//   slide: {
+//     flex: 1,
+//     justifyContent: 'center',
+//     backgroundColor: 'transparent',
+//     borderRadius: 8,
+//   },
+//   sliderImage: {
+//     height: '100%',
+//     width: '100%',
+//     alignSelf: 'center',
+//     borderRadius: 8,
+//   },
+// });
 
 export default function Cafeterias() {
   return (
@@ -48,3 +96,104 @@ export default function Cafeterias() {
     </Tab.Navigator>
   );
 }
+
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  sliderContainer: {
+    height: 200,
+    width: '90%',
+    marginTop: 10,
+    justifyContent: 'center',
+    alignSelf: 'center',
+    borderRadius: 8,
+  },
+
+  wrapper: {},
+
+  slide: {
+    flex: 1,
+    justifyContent: 'center',
+    backgroundColor: 'transparent',
+    borderRadius: 8,
+  },
+  sliderImage: {
+    height: '100%',
+    width: '100%',
+    alignSelf: 'center',
+    borderRadius: 8,
+  },
+  categoryContainer: {
+    flexDirection: 'row',
+    width: '90%',
+    alignSelf: 'center',
+    marginTop: 25,
+    marginBottom: 10,
+  },
+  categoryBtn: {
+    flex: 1,
+    width: '30%',
+    marginHorizontal: 0,
+    alignSelf: 'center',
+  },
+  categoryIcon: {
+    borderWidth: 0,
+    alignItems: 'center',
+    justifyContent: 'center',
+    alignSelf: 'center',
+    width: 70,
+    height: 70,
+    backgroundColor: '#fdeae7' /* '#FF6347' */,
+    borderRadius: 50,
+  },
+  categoryBtnTxt: {
+    alignSelf: 'center',
+    marginTop: 5,
+    color: '#de4f35',
+  },
+  cardsWrapper: {
+    marginTop: 20,
+    width: '90%',
+    alignSelf: 'center',
+  },
+  card: {
+    height: 100,
+    marginVertical: 10,
+    flexDirection: 'row',
+    shadowColor: '#999',
+    shadowOffset: {width: 0, height: 1},
+    shadowOpacity: 0.8,
+    shadowRadius: 2,
+    elevation: 5,
+  },
+  cardImgWrapper: {
+    flex: 1,
+  },
+  cardImg: {
+    height: '100%',
+    width: '100%',
+    alignSelf: 'center',
+    borderRadius: 8,
+    borderBottomRightRadius: 0,
+    borderTopRightRadius: 0,
+  },
+  cardInfo: {
+    flex: 2,
+    padding: 10,
+    borderColor: '#ccc',
+    borderWidth: 1,
+    borderLeftWidth: 0,
+    borderBottomRightRadius: 8,
+    borderTopRightRadius: 8,
+    backgroundColor: '#fff',
+  },
+  cardTitle: {
+    fontWeight: 'bold',
+  },
+  cardDetails: {
+    fontSize: 12,
+    color: '#444',
+  },
+});
