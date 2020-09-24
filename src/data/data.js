79f6e6fd2b7dfcfa9fd454d8c1326eeeb2b4fe6d
jusheen <jusheen9@gmail.com>
2020-09-23 23:40:19 -0700

    exported hardcoded data to external file temporarily until I integrate firebase

---
+++ src/data/data.js
@@ -0,0 +1,61 @@
+module.exports = {
+  cafe3Posts: [
+    {
+      id: '1',
+      name: 'Ju Sheen Kim',
+      text: "Anyone down for Cafe 3 at noon?",
+      avatar: require('../images/jusheen.jpg'),
+      timestamp: '10:17am'
+    },
+    {
+      id: '2',
+      name: 'Nilay Neeranjun',
+      text: "Down, meet at unit 3? I live in Priestley.",
+      avatar: require('../images/nilay.jpg'),
+      timestamp: '10:30am'
+    },
+    {
+      id: '3',
+      name: 'Ju Sheen Kim',
+      text: "Me too! Sounds like a plan. I'll be by the benches disguised in a blue cap and white shirt.",
+      avatar: require('../images/jusheen.jpg'),
+      timestamp: '10:31am'
+    },
+    {
+      id: '4',
+      name: 'Raghav Jalan',
+      text: "Mind if I join?",
+      avatar: require('../images/ragz.jpg'),
+      timestamp: '11:47am',
+    },
+    {
+      id: '5',
+      name: 'Nilay Neeranjun',
+      text: "Slide bro",
+      avatar: require('../images/nilay.jpg'),
+      timestamp: '11:47am'
+    },
+    {
+      id: '6',
+      name: 'Youssef Essanaa',
+      text: "How's the food today?",
+      avatar: require('../images/youss.jpg'),
+      timestamp: '12:33pm'
+    },
+    {
+      id: '7',
+      name: 'Raghav Jalan',
+      text: "It's lit",
+      avatar: require('../images/ragz.jpg'),
+      timestamp: '12:56pm',
+      image: require('../images/cafe3_food.png')
+    },
+    {
+      id: '8',
+      name: 'Nick Tokunaga',
+      text: "That looks so good...who's down to go tonight after my bio lecture at 6:30?",
+      avatar: require('../images/toku.jpg'),
+      timestamp: '4:13pm'
+    }
+  ],
+}
