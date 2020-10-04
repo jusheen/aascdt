cbbede30eb11310d57a26ce5bfdf953a5f7e8f23
jusheen <jusheen9@gmail.com>
2020-10-04 12:06:29 -0700

    created form button

--- src/components/FormButton.js
+++ src/components/FormButton.js
@@ -2,24 +2,31 @@ import React from 'react';
 import { Text, TouchableOpacity, StyleSheet } from 'react-native';
 import {windowHeight, windowWidth} from '../utils/Dimensions';
 
-const FormButton = () => {
+const FormButton = ({ buttonTitle, ...rest }) => {
   return (
-    <View style={styles.container}>
-      <Text></Text>
-      <Button
-        title="Click Here"
-        onPress={() => alert('Button Clicked')}
-      />
-    </View>
-  )
+    <TouchableOpacity style={styles.buttonContainer} {...rest}>
+      <Text style={styles.buttonText}>{buttonTitle}</Text>
+    </TouchableOpacity>
+  );
 }
 
 export default FormButton;
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
+  buttonContainer: {
+    marginTop: 10,
+    width: '100%',
+    height: windowHeight / 15,
+    backgroundColor: '#2e64e5',
+    padding: 10,
     alignItems: 'center',
-    justifyContent: 'center'
+    justifyContent: 'center',
+    borderRadius: 3,
   },
-})
+  buttonText: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    color: '#ffffff',
+    fontFamily: 'Lato-Regular',
+  },
+});
